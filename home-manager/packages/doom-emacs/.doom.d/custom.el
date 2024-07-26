239a2989d7d611030a2d6f657e4ab74d136f0ea7
lily <contact@lilyvex.dev>
2024-07-26 18:55:27 -0400

    conf: doom-emacs

---
+++ home-manager/packages/doom-emacs/.doom.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("4825b816a58680d1da5665f8776234d4aefce7908594bea75ec9d7e3dc429753" "8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" "da75eceab6bea9298e04ce5b4b07349f8c02da305734f7c0c8c6af7b5eaa9738" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "d481904809c509641a1a1f1b1eb80b94c58c210145effc2631c1a7f2e4a2fdf4" "77fff78cc13a2ff41ad0a8ba2f09e8efd3c7e16be20725606c095f9a19c24d3d" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- home-manager/packages/emacs.nix
+++ home-manager/packages/emacs.nix
@@ -5,4 +5,9 @@
     enable = true;
     package = pkgs.emacs;
   };
+
+  # environment.variables = {
+  #   # Point Emacs to the source controlled configuration files.
+  #   EMACSLOADPATH = builtins.getEnv "HOME" + "/.dotfiles/home-manager/packages/doom-emacs";
+  # };
 }
