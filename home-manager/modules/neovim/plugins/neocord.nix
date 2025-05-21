40e7251cf30655ede48d52a4ff33177fbc48f9df
lily <contact@lilyvex.dev>
2025-05-21 01:00:17 -0400

    add(nixvim): custom neocord logo

--- home-manager/modules/neovim/plugins/neocord.nix
+++ home-manager/modules/neovim/plugins/neocord.nix
@@ -1,6 +1,6 @@
 {...}: {
   programs.nixvim.plugins.neocord.enable = true;
   programs.nixvim.plugins.neocord.settings = {
-    show_line_number = true;
+    logo = "https://i.imgur.com/ILfUuRc.png";
   };
 }
